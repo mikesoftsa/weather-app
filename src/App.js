e54c691fa8fd8b32c679487d57d6e65329484bd6
Miguel Triana <mikesoft241184@gmail.com>
2018-12-12 00:13:21 -0500

    10. Creación de functional component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import WeatherLocation from './components/WeatherLocation'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Weather App (Aplicacion del clima)
+        <WeatherLocation></WeatherLocation>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+//componente funcional
+const WeatherLocation = () => (
+    <diV> Weather Location</diV>
+);
+
+export default WeatherLocation;
