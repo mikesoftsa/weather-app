0057d208879b82c7c341b2f1d950f7700ccf2404
Miguel Triana <mikesoft241184@gmail.com>
2019-02-12 01:23:39 -0500

    Sección 11, Clase 100 Generación de ForecastItems en base a los datos

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -47,9 +47,15 @@ class ForecastExtended extends Component {
         );
     }
 
-    renderForecastItemDays(){
-        return <h1>Render Items</h1>;
-        //return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>))
+    renderForecastItemDays(forecastData){
+        return forecastData.map(forecast => (
+        <ForecastItem 
+            key={`${forecast.weekDay}${forecast.hour}`}
+            weekDay={forecast.weekDay} 
+            hour={forecast.hour} 
+            data={forecast.data}>
+        </ForecastItem>
+        ))
     }
 
     renderProgress(){
@@ -63,7 +69,7 @@ class ForecastExtended extends Component {
             <div>
                <h2 className="forecast-title"> Pronostico Extendido para { city } </h2> 
                {forecastData ? 
-                    this.renderForecastItemDays(): 
+                    this.renderForecastItemDays(forecastData): 
                     this.renderProgress()
                 }
             </div>
