855f2071f1fe60e8ff09632c759ec6292fa2e2d9
Miguel Triana <mikesoft241184@gmail.com>
2019-02-11 23:40:47 -0500

    Sección 11, Clase 92 Primer paso de refactorización de ForecastExtended: Indicador carga inicial

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -19,16 +19,32 @@ const data = {
 
 class ForecastExtended extends Component {
 
+    constructor(){
+        super();
+        this.state = { 
+            forecastData: null,
+        }
+    }
+
     renderForecastItemDays(){
-        return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>))
+        return "Render Items";
+        //return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>))
+    }
+
+    renderProgress(){
+        return <h3>Cargando Pronostico extendido...</h3>;
     }
 
     render(){
         const { city } = this.props;
+        const { forecastData } = this.state;
         return (
             <div>
                <h2 className="forecast-title"> Pronostico Extendido para { city } </h2> 
-               {this.renderForecastItemDays()}
+               {forecastData ? 
+                    this.renderForecastItemDays(): 
+                    this.renderProgress()
+                }
             </div>
         );
     }
