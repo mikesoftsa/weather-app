d59e939632eb510abea923f7fd5bb12180a84b54
Miguel Triana <mikesoft241184@gmail.com>
2019-02-17 23:50:23 -0500

    Sección 13, Clase 122 Creación de container LocationListContainer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Paper from '@material-ui/core/Paper';
 import AppBar from  '@material-ui/core/AppBar';
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Grid, Col, Row } from 'react-flexbox-grid';
-import PropTypes from "prop-types";
 import './App.css';
-import LocationList from './components/LocationList';
+import LocationListContainer from './containers/LocationListContainer';
 import ForecastExtended from "./components/ForecastExtended";
-
-import { setCity } from "./actions";
 
 const cities = [
   'Buenos Aires,ar',
@@ -28,11 +24,6 @@ class App extends Component {
     this.state = { city: null}
   }
 
-  handleSelectedLocation = city =>{
-    this.setState({city});
-    console.log(`handleSelectedLocation ${city}`);
-    this.props.setCity(city);
-  }
   render() {
     const { city } = this.state;
     return (
@@ -48,11 +39,7 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12} md={6}>
-            <LocationList 
-              cities={cities} 
-              onSelectedLocation={this.handleSelectedLocation}
-            >
-            </LocationList>
+            <LocationListContainer cities={cities} ></LocationListContainer>
           </Col>
           <Col xs={12} md={6}>
           <Paper elevation={4}>
@@ -73,13 +60,6 @@ class App extends Component {
   }
 }
 
-App.propTypes = { 
-  setCity: PropTypes.func.isRequired,
-}
 
-const mapDispatchToProps = dispatch => ({
-  setCity: value => dispatch(setCity(value))
-});
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
 
---
+++ src/containers/LocationListContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import LocationList from "./../components/LocationList";
+import { setCity } from "./../actions";
+
+class LocationListContainer extends Component {
+    handleSelectedLocation = city =>{
+        this.props.setCity(city);
+    }
+    render() {
+        return (
+            <LocationList cities={this.props.cities} 
+              onSelectedLocation={this.handleSelectedLocation} ></LocationList>
+        );
+    }
+}
+
+LocationListContainer.propTypes = {
+    setCity: PropTypes.func.isRequired,
+    cities: PropTypes.array.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+    setCity: value => dispatch(setCity(value))
+  });
+
+export default connect(null, mapDispatchToProps)(LocationListContainer);;
