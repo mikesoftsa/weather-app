1561708811e8b1dfd3d919488b2c0e69c2896dd0
Miguel Triana <mikesoft241184@gmail.com>
2019-02-19 01:58:01 -0500

    Sección 14, Clase 137 Selectores, completo

--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ForecastExtended from '../components/ForecastExtended';
 import { connect } from 'react-redux';
-import { getForecastDataFromCities } from "./../reducers/cities";
+import { getForecastDataFromCities, getCity } from "./../reducers";
 
 
 class ForecastExtendedContainer extends Component {
@@ -20,7 +20,7 @@ ForecastExtendedContainer.propTypes = {
     forecastData: PropTypes.array,
 };
 
-const mapStateToProps = state => ({ city: state.city, forecastData: getForecastDataFromCities(state.cities, state.city) })
+const mapStateToProps = state => ({ city: getCity(state), forecastData: getForecastDataFromCities(state) })
 
 export default connect(mapStateToProps, null)(ForecastExtendedContainer);
 
