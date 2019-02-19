2dd59cd34a3d03506ea87fdf2a3b2a9b7a0418f5
Miguel Triana <mikesoft241184@gmail.com>
2019-02-19 01:28:31 -0500

    Sección 14, Clase 135 Simplificación de class component a functional component

--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -5,18 +5,20 @@ import { connect } from 'react-redux';
 
 class ForecastExtendedContainer extends Component {
     render() {
+        const { city, forecastData } = this.props;
         return (
-            this.props.city && 
-            <ForecastExtended city={this.props.city} />
+            city && 
+            <ForecastExtended city={city} forecastData={forecastData} />
         );
     }
 }
 
 ForecastExtendedContainer.propTypes = {
     city: PropTypes.string.isRequired, /*ptsr para que cree por defecto proptype string requerido*/
+    forecastData: PropTypes.array.isRequired,
 };
 
-const mapStateToProps = ({ city }) => ({ city })
+const mapStateToProps = ({ city, cities }) => ({ city, forecastData: cities[city] && cities[city].forecastData })
 
 export default connect(mapStateToProps, null)(ForecastExtendedContainer);
 
