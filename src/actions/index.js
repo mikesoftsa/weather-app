383e42e06722ee6091414c6afbcbee00140b6391
Miguel Triana <mikesoft241184@gmail.com>
2019-02-19 23:04:36 -0500

    Sección 14, Clase 141 Creación de Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,22 @@
 import transformForecast from "./../services/transformForecast";
+import transformWeather from "./../services/transformWeather";
 
 export const SET_CITY = 'SET_CITY';
 export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
-export const SET_WEATHER = 'SET_WEATHER';
+
+export const GET_WEATHER_CITY = 'GET_WEATHER_CITY';
+export const SET_WEATHER_CITY = 'SET_WEATHER_CITY';
 
 
 const setCity = payload => ({ type: SET_CITY, payload});
-const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload })
+const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload });
+
+const getWeatherCity = payload => ({ type: GET_WEATHER_CITY, payload});
+const setWeatherCity = payload => ({ type: SET_WEATHER_CITY, payload})
 
 const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
 const url = "http://api.openweathermap.org/data/2.5/forecast";
+const url_weather = "http://api.openweathermap.org/data/2.5/weather";
 
 export const setSelectedCity = payload => {
     return dispatch => {
@@ -34,15 +41,21 @@ export const setSelectedCity = payload => {
 };
 
 export const setWeather = payload => {
-    /*const api_weather = getUrlWeatherByCity(this.state.city);
-    fetch(api_weather).then(resolve => {            
-        return resolve.json();
-    }).then( data => {
-        console.log("Resultado del handleUpdateClick");
-        const newWeather = transformWeather(data);
-        console.log(newWeather);            
-        this.setState({
-            data: newWeather
+
+    return dispatch => {
+        payload.forEach( city => {
+
+            dispatch(getWeatherCity(city));
+
+            const api_weather = `${url_weather}?q=${city}&appid=${api_key}`;
+            fetch(api_weather).then(data => {            
+                return data.json();
+            }).then( weather_data => {
+                const data = transformWeather(weather_data);
+
+                dispatch(setWeatherCity(city, data));
+            });
         })
-    });        */
+   
+    }
 }
